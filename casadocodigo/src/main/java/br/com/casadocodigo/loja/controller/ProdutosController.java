cfeb77372ab497b20eeed3eb62a908e207e15b4a
rafael <rafael@rafael-Lenovo-G40-70>
2018-01-17 01:42:47 -0200

    Listando os produtos
    encoding
    
    rota duplicada

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controller/ProdutosController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controller/ProdutosController.java
@@ -3,6 +3,7 @@ package br.com.casadocodigo.loja.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.dao.ProdutoDao;
@@ -10,19 +11,25 @@ import br.com.casadocodigo.loja.model.Produto;
 import br.com.casadocodigo.loja.model.TipoPreco;
 
 @Controller
+@RequestMapping("/produtos")
 public class ProdutosController {
 	
 	@Autowired
 	private ProdutoDao produtoDao;
 
-	@RequestMapping("/produtos/form")
+	@RequestMapping("/form")
 	public ModelAndView form() {
 		return new ModelAndView("produtos/form").addObject("tipos", TipoPreco.values());
 	}
 	
-	@RequestMapping("/produtos")
+	@RequestMapping(method = RequestMethod.POST)
 	public String adiciona(Produto produto) {
 		produtoDao.adiciona(produto);
 		return "produtos/ok";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView lista() {
+		return new ModelAndView("produtos/lista").addObject("produtos", produtoDao.lista());
 	}
 }
